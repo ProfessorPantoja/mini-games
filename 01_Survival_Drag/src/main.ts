f411ebf295522c3e3a6f83425216411ad2cf4805
Fabio Pantoja <artpantoja@gmail.com>
2026-07-13 15:38:55 -0300

    feat: adiciona index.html e bootstrap Phaser

---
+++ 01_Survival_Drag/src/main.ts
@@ -0,0 +1,26 @@
+import Phaser from 'phaser';
+import { BootScene } from './scenes/BootScene';
+import { GameScene } from './scenes/GameScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  width: 800,
+  height: 600,
+  backgroundColor: '#2d4a3e',
+  pixelArt: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+      debug: false,
+    },
+  },
+  scene: [BootScene, GameScene],
+};
+
+new Phaser.Game(config);
